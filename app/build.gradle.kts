132d85da760c90187d18bd33675be607486fcd17
IsraelMerlyn <merlyn142a@gmail.com>
2024-11-11 13:25:57 -0600

    Tasks #0.0.2 Editando Estructura del codigo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.example.juegobotella"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.example.juegobotella"
